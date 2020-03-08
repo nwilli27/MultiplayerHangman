e943abbed47f41a9eb7a7673f25904100a412a07
Nolan Williams <nwilli27@my.westga.edu>
2020-03-08 18:22:46 -0400

    Initial commit, setup base classes.

---
+++ CS4225P1Group2_Server/src/model/Stickman.java
@@ -0,0 +1,60 @@
+package model;
+
+public class Stickman {
+
+	private boolean head;
+	private boolean body;
+	private boolean leftArm;
+	private boolean rightArm;
+	private boolean leftLeg;
+	private boolean rightLeg;
+	
+	public boolean isHead() {
+		return this.head;
+	}
+	
+	public void setHead(boolean head) {
+		this.head = head;
+	}
+	
+	public boolean isBody() {
+		return this.body;
+	}
+	
+	public void setBody(boolean body) {
+		this.body = body;
+	}
+	
+	public boolean isLeftArm() {
+		return this.leftArm;
+	}
+	
+	public void setLeftArm(boolean leftArm) {
+		this.leftArm = leftArm;
+	}
+	
+	public boolean isRightArm() {
+		return this.rightArm;
+	}
+	
+	public void setRightArm(boolean rightArm) {
+		this.rightArm = rightArm;
+	}
+	
+	public boolean isLeftLeg() {
+		return this.leftLeg;
+	}
+	
+	public void setLeftLeg(boolean leftLeg) {
+		this.leftLeg = leftLeg;
+	}
+	
+	public boolean isRightLeg() {
+		return this.rightLeg;
+	}
+	
+	public void setRightLeg(boolean rightLeg) {
+		this.rightLeg = rightLeg;
+	}
+
+}
