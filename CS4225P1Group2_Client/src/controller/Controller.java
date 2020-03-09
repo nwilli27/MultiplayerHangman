a9c4b7572f7ba40e63ad2f7c9f93aecfb6644f37
cbedrosian1 <cbedrosian1@gmail.com>
2020-03-09 10:57:53 -0400

    Made controller a singleton class

--- CS4225P1Group2_Client/src/controller/Controller.java
+++ CS4225P1Group2_Client/src/controller/Controller.java
@@ -1,5 +1,32 @@
 package controller;
 
+import java.util.ArrayList;
+
+
 public class Controller {
 
+	
+	
+	private static Controller instance;
+
+
+	private Controller() {
+
+	}
+
+	/**
+	 * Returns an instance of the data class
+	 * 
+	 * @precondition none
+	 * @postcondition none
+	 * @return the instance of the data class
+	 */
+	public static Controller getInstance() {
+		if (instance == null) {
+			instance = new Controller();
+		}
+
+		return instance;
+	}
+	
 }
