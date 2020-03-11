ce8ee4c186bf58c50a204684c754c44c54f769f0
nwilli27 <nwilli27@my.westga.edu>
2020-03-11 00:18:42 -0400

    Updated Message class to carry isCompleted condition.

--- CS4225P1Group2_Server/src/model/Message.java
+++ CS4225P1Group2_Server/src/model/Message.java
@@ -6,13 +6,14 @@ import enums.MessageType;
 
 /**
  * Encapsulates a message to be serializable.
- * @author Nolan W, Carsen B, Tristen R
+ * @author Nolan W, Carson B, Tristen R
  */
 public class Message implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	private String message;
 	private MessageType type;
+	private boolean isCompleted;
 
 	/**
 	 * Constructor to initialize a message.
@@ -39,4 +40,23 @@ public class Message implements Serializable {
 	public MessageType getType() {
 		return this.type;
 	}
+	
+	/**
+	 * Returns if the message has been completed.
+	 * @return true if completed; otherwise false
+	 */
+	public boolean isCompleted() {
+		return this.isCompleted;
+	}
+	
+	/**
+	 * Sets if the message has been completed.
+	 * @precondition none
+	 * @postcondition this.isCompleted = condition
+	 * @param condition the condition to set
+	 */
+	public void setIsCompleted(boolean condition) {
+		this.isCompleted = condition;
+	}
+	
 }
